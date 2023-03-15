2152c169e7eedb71d6310be99d549c8fbc5b3b3e
Juan Fernando Rojas Cortes <jrojas@cafetosoftware.com>
2023-03-15 17:43:09 -0500

    polishment

--- src/app.py
+++ src/app.py
@@ -5,5 +5,5 @@ server = Flask(__name__)
 server.debug = True
 server.register_blueprint(document_blueprint, url_prefix="/document")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server.run(host="0.0.0.0", port=8080)
